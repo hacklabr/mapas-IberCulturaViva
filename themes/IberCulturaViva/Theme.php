53aece84901e52746fb093ed6693977545d18ff9
Leonardo <leopiccionia@gmail.com>
2018-07-30 12:38:34 -0300

    Update images.

--- themes/IberCulturaViva/Theme.php
+++ themes/IberCulturaViva/Theme.php
@@ -51,9 +51,11 @@ class Theme extends BaseV1\Theme{
     protected function _publishAssets() {
         $this->asset('img/home--agents.jpg', false);
         $this->asset('img/home--developers.jpg', false);
+        $this->asset('img/home--events.jpg', false);
         $this->asset('img/home--intro.jpg', false);
         $this->asset('img/home--opportunities.jpg', false);
         $this->asset('img/home--projects.jpg', false);
+        $this->asset('img/home--spaces.jpg', false);
 
         $this->jsObject['assets']['logo-instituicao'] = $this->asset('img/logo-instituicao.png', false);
     }
