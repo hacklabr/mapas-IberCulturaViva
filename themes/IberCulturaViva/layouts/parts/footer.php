58f3c84a636f5d1a4e85683f35e45caa36f50a2d
Leonardo <leopiccionia@gmail.com>
2018-08-09 11:39:57 -0300

    Footer (WIP).

--- themes/IberCulturaViva/layouts/parts/footer.php
+++ themes/IberCulturaViva/layouts/parts/footer.php
@@ -1,10 +1,14 @@
 <footer id="main-footer">
     <div class="column">
-        <p><a href="http://iberculturaviva.org/"><img src="<?php $this->asset('img/logo-site.svg'); ?>" alt="IberCultura Viva"></a></p>
-        <p><img src="<?php $this->asset('img/bandeiras-paises-membros.png'); ?>"></p>
+        <p class="logo-iberculturaviva"><a href="http://iberculturaviva.org/"><img src="<?php $this->asset('img/logo-site.svg'); ?>" alt="IberCultura Viva"></a></p>
+        <p class="social-icons">
+            <a href="https://www.facebook.com/iberculturaviva">&#xe093;</a>
+            <a href="https://twitter.com/iberculturaviva">&#xe094;</a>
+            <a href="#">&#xe09c;</a>
+            <a href="#">&#xe0a3;</a>
+        </p>
     </div>
     <div class="column">
-        <p>Desenvolvido por</p>
-        <p><a href="https://hacklab.com.br/"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Hacklab"></a></p>
+        <p class="logo-hacklab">Desenvolvido pelo <a href="https://hacklab.com.br"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Hacklab"></a></p>
     </div>
 </footer>
