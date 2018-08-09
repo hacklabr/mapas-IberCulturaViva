65814785d31b218ca5bf96b2a62fbe531031b460
Leonardo <leopiccionia@gmail.com>
2018-08-09 10:23:05 -0300

    Fix footer.

--- themes/IberCulturaViva/layouts/parts/footer.php
+++ themes/IberCulturaViva/layouts/parts/footer.php
@@ -1,12 +1,10 @@
 <footer id="main-footer">
-    <div class="row">
-        <div class="col-6">
-            <p><a href="http://iberculturaviva.org/"><img src="<?php $this->asset('img/logo-site.svg'); ?>" alt="IberCultura Viva"></a></p>
-            <p><img src="<?php $this->asset('img/bandeiras-paises-membros.png'); ?>"></p>
-        </div>
-        <div class="col-6">
-            <p>Desenvolvido por</p>
-            <p><a href="https://hacklab.com.br/"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Hacklab"></a></p>
-        </div>
+    <div class="column">
+        <p><a href="http://iberculturaviva.org/"><img src="<?php $this->asset('img/logo-site.svg'); ?>" alt="IberCultura Viva"></a></p>
+        <p><img src="<?php $this->asset('img/bandeiras-paises-membros.png'); ?>"></p>
+    </div>
+    <div class="column">
+        <p>Desenvolvido por</p>
+        <p><a href="https://hacklab.com.br/"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Hacklab"></a></p>
     </div>
 </footer>
