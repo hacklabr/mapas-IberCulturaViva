80a3322ae4120329a83ea2bd59c8f119bd841c88
Leonardo <leopiccionia@gmail.com>
2018-08-09 13:09:44 -0300

    A11y improvements.

--- themes/IberCulturaViva/layouts/parts/footer.php
+++ themes/IberCulturaViva/layouts/parts/footer.php
@@ -2,13 +2,13 @@
     <div class="column">
         <p class="logo-iberculturaviva"><a href="http://iberculturaviva.org/"><img src="<?php $this->asset('img/logo-site.svg'); ?>" alt="IberCultura Viva"></a></p>
         <p class="social-icons">
-            <a href="https://www.facebook.com/iberculturaviva">&#xe093;</a>
-            <a href="https://twitter.com/iberculturaviva">&#xe094;</a>
-            <a href="#">&#xe09c;</a>
-            <a href="#">&#xe0a3;</a>
+            <a href="https://www.facebook.com/iberculturaviva" aria-label="Facebook">&#xe093;</a>
+            <a href="https://twitter.com/iberculturaviva" aria-label="Twitter">&#xe094;</a>
+            <a href="#" aria-label="Vimeo">&#xe09c;</a>
+            <a href="#" aria-label="YouTube">&#xe0a3;</a>
         </p>
     </div>
     <div class="column">
-        <p class="logo-hacklab">Desenvolvido pelo <a href="https://hacklab.com.br"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="Hacklab"></a></p>
+        <p class="logo-hacklab">Desenvolvido pelo <a href="https://hacklab.com.br"><img src="<?php $this->asset('img/logo-hacklab.svg'); ?>" alt="hacklab/"></a></p>
     </div>
 </footer>
