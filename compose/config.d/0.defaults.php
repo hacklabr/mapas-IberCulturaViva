3ed8eb8adafd5e5e7fb3cf90c10e2a491da190f5
Leonardo <leopiccionia@gmail.com>
2018-08-13 18:49:48 -0300

    Set site name and description.

--- compose/config.d/0.defaults.php
+++ compose/config.d/0.defaults.php
@@ -35,8 +35,9 @@ switch(env('LOG_LEVEL', 'NOTICE')){
 }
 
 return [
-    'app.siteName' => env('SITE_NAME', 'Mapas Culturais'),
-    'app.siteDescription' => env('SITE_DESCRIPTION', ''),
+    'app.siteName' => env('SITE_NAME', 'Mapa IberCultura Viva'),
+    'app.siteDescription' => \MapasCulturais\i::__("O Mapa IberCultura Viva é a plataforma livre, gratuita e colaborativa de mapeamento do programa de cooperação IberCultura Viva. Neste espaço, representantes de organizações culturais comunitárias e povos originários podem se registrar como agentes culturais, difundir eventos, cadastrar projetos e inscrever-se nos editais e concursos publicados.<br/><br/>
+    A informação coletada pelo Mapa IberCultura Viva será utilizada para a construção/consolidação de indicadores culturais que fortalecerão os sistemas de informação cultural dos países membros do programa (Argentina, Brasil, Chile, Costa Rica, El Salvador, Equador, Espanha, Guatemala, México, Peru e Uruguai)."),
 
     'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\BaseV1'),
 
