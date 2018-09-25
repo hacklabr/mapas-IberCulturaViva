40e7abf0f09c2128c7168880eb951ab9e4b663a9
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2018-09-25 15:28:14 -0300

    exporta o país do proponente na tabela de inscritos

--- compose/config.d/2.registration.php
+++ compose/config.d/2.registration.php
@@ -8,4 +8,31 @@ return [
         'type' => 1,
         'requiredProperties' => array('documento', 'endereco', 'En_Pais', 'dataDeNascimento', 'genero', 'emailPrivado', 'telefone1')
     ),
+
+    'registration.propertiesToExport' => array(
+        'id',
+        'name',
+        'nomeCompleto',
+        'documento',
+        'dataDeNascimento',
+        'location',
+        'endereco',
+        'En_CEP',
+        'En_Nome_Logradouro',
+        'En_Num',
+        'En_Complemento',
+        'En_Bairro',
+        'En_Municipio',
+        'En_Estado',
+        'En_Pais',
+        'telefone1',
+        'telefone2',
+        'telefonePublico',
+        'emailPrivado',
+        'emailPublico',
+        'site',
+        'googleplus',
+        'facebook',
+        'twitter'
+    ),
 ];
