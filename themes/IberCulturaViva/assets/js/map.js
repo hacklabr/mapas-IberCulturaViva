e325907252ea6cc2e75f716eb736176b221d45be
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2019-06-24 10:48:59 -0300

    muda a url dos tiles para o mapa deixar de ser exibido

--- themes/IberCulturaViva/assets/js/map.js
+++ themes/IberCulturaViva/assets/js/map.js
@@ -66,7 +66,8 @@
             options.zoom = defaultZoom;
             options.zoomControl = false;
             options.minZoom = config.zoomMin;
-            var openStreetMap = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=gn', {
+            // var openStreetMap = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=gn', {
+                var openStreetMap = L.tileLayer('https://disabled/osm-intl/{z}/{x}/{y}.png?lang=gn', {
                 attribution: 'Dados e Imagens &copy; <a href="http://www.openstreetmap.org/copyright">Contrib. OpenStreetMap</a>, ',
                 maxZoom: config.zoomMax
             });
--- themes/IberCulturaViva/assets/js/single-subsite.js
+++ themes/IberCulturaViva/assets/js/single-subsite.js
@@ -11,7 +11,8 @@ $(function(){
         center: new L.LatLng(obj.latitude || mapsDefaults.latitude, obj.longitude || mapsDefaults.longitude)
     };
     
-    var openStreetMap = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=gn', {
+    // var openStreetMap = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=gn', {
+        var openStreetMap = L.tileLayer('https://disabled/osm-intl/{z}/{x}/{y}.png?lang=gn', {
         attribution: 'Dados e Imagens &copy; <a href="http://www.openstreetmap.org/copyright">Contrib. OpenStreetMap</a>, ',
         maxZoom: config.zoomMax,
         minZoom: config.zoomMin
