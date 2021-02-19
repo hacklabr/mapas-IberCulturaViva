d567037072a3f6a32b282da8f16486f5cbd70ea5
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-02-18 23:53:24 -0300

    atualiza o mapasculurais para a versão v5.0.0-RC

---
+++ compose/common/config.d/maps.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'maps.tileServer' => 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=es',
+    // 'maps.tileServer' => 'http://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+    
+];
