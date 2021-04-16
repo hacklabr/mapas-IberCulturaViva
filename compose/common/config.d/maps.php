e99000e5cc4c72837c4e6a3eb6f1a7c79a90dafd
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2021-04-16 00:32:08 -0300

    remove configuração hardcoded do tileserver

--- compose/common/config.d/maps.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'maps.tileServer' => 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=es',
-    // 'maps.tileServer' => 'http://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-    
-];
