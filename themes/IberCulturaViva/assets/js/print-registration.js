12d4f749585cc2364f0916d51bacc398b3b221f5
Alex Uragawa <chiozo.m+dev@gmail.com>
2021-12-13 16:45:36 -0300

    remove rodapé da impressão da inscrição

--- themes/IberCulturaViva/assets/js/print-registration.js
+++ themes/IberCulturaViva/assets/js/print-registration.js
@@ -1,19 +1,11 @@
 $(function () {
     // initialisation
-    // const $footer = $("footer");
-    // $footer.before("<div id=\"empty-footer\">.</div>");
     $("#main-header").removeClass("sombra");
     // trigger the print window
     setTimeout(function () {
-        // $(window).resize();
         window.print();
         return;
     }, 500);
-    // adjust height of empty-footer
-    // $(window).resize(function () {
-    //     $("#empty-footer").height($footer.height());
-    //     return;
-    // });
     // adjust header on scroll
     $(window).scroll(function () {
         $("#main-header")[0].style.top = 0;
