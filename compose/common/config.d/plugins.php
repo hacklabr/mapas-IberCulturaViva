46c48eb84c143531e73fd2a9f0595b5529ba7365
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2022-05-10 15:11:48 -0300

    instala plugin LocationPatch

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -6,6 +6,23 @@ return [
         'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
         'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
         
-        'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ]
+        'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
+
+        "LocationPatch" => [
+            "namespace" => "LocationPatch",
+            "config" => [
+                /**
+                 * Flag para habilitar o plugin.
+                 * Tipo: boolean
+                 */
+                "enable" => env("LOCATION_PATCH_ENABLE", false),
+                /**
+                 * Ponto de corte para seleção das entidades. São apenas consideradas
+                 * as entidades atualizadas antes deste timestamp.
+                 * Formato: yyyyMMddHHmmss
+                 */
+                "cutoff" => env("LOCATION_PATCH_CUTOFF", "20220510120000"),
+            ],
+        ],
     ]
 ];
